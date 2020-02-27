5b5a65990f2cb12a1720ee572aef5db4de23987b
nafin147 <43007741+nafin147@users.noreply.github.com>
2020-02-26 20:12:11 -0500

    Created phone verification API #23

--- src/main/java/com/example/AnonMe/api/PhoneVerificationController.java
+++ src/main/java/com/example/AnonMe/api/PhoneVerificationController.java
@@ -9,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path="/verify")
 public class PhoneVerificationController {
+
     PhoneVerification verification = new PhoneVerification();
+
     @GetMapping(path="/phoneVerification")
     public int initialize(@RequestParam String phoneNumber){
         return verification.sendCode(phoneNumber);
     }
+
     @GetMapping(path = "/checkCode")
     public int checkCode(@RequestParam String phoneNumber, @RequestParam String code){
         return verification.checkVerification(phoneNumber, code);
