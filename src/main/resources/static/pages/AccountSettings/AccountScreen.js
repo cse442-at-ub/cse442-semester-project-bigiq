33f541ea9eb9be07eaded90e85ef27b2d5cb9576
Rickx777 <conanwon@buffalo.edu>
2020-04-12 21:44:49 -0400

    Revert "Revert "Merge branch 'CommentRendering' into Development""
    This reverts commit e7b3a8bb033571cc89d300b3f945f676e796c48e.

---
+++ src/main/resources/static/pages/AccountSettings/AccountScreen.js
@@ -0,0 +1,186 @@
+import React from 'react';
+import {
+    Image,
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    AsyncStorage,
+    FlatList,
+    Platform,
+    TouchableWithoutFeedback
+} from 'react-native';
+import {postByAuthor} from "../../fetches/PostFetch";
+
+export default class AccountScreen extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            screenName: '',
+            doneLoading: false,
+            data: [],
+        }
+    }
+
+    fetchUserPost = () =>{
+        postByAuthor(this.state.screenName).then( dataAPI =>
+            this.setState({data : dataAPI})
+        );
+
+    };
+    settingScreen = () =>{
+        this.props.navigation.navigate('SettingScreen');
+    };
+    componentDidMount() {
+        AsyncStorage.getItem('screenName').then((token) => {
+            this.setState({
+                screenName: token,
+                doneLoading: true
+            });
+        });
+        const { navigation } = this.props;
+        navigation.addListener("focus", () => {
+            this.fetchUserPost();
+        });
+    };
+    _listEmptyComponent = () => {
+        return (
+            <View style={{alignItems: 'center', justifyContent: 'center', height: 300}}>
+                <Text>Looks like you haven't posted anything.</Text>
+            </View>
+        )
+    };
+    render() {
+        let that = this;
+        return (
+            <View style={{ alignItems: 'center'}}>
+                <TouchableOpacity style={{ position: 'absolute', right: '5%', top: '5%'}} onPress={() => that.settingScreen()}>
+                    <Image style={styles.settingIcon}
+                           source={require('../../assets/settingIcon.png')}/>
+                </TouchableOpacity>
+                <Image style={styles.avatar}
+                       source={require('../../assets/avatars/1.png')}/>
+                <View style={styles.nameContainer}>
+                    <Text style={{color:'white'}}>{that.state.screenName}</Text>
+                </View>
+                <View style={styles.statsContainer}>
+                    <View style={styles.statsView}>
+                        <Text style={styles.statsText}>0</Text>
+                        <Text style={{color: 'gray'}}>Posts</Text>
+                    </View>
+                    <View style={styles.statsView}>
+                        <Text style={styles.statsText}>0</Text>
+                        <Text style={{color: 'gray'}}>Likes</Text>
+                    </View>
+                    <View style={styles.statsView}>
+                        <Text style={styles.statsText}>0</Text>
+                        <Text style={{color: 'gray'}}>Following</Text>
+                    </View>
+                </View>
+                <View style={{marginVertical: 20}}>
+                    <Text style={{color: 'gray'}}>Posts by {that.state.screenName}</Text>
+                </View>
+                <View>
+                    <FlatList
+                        showsHorizontalScrollIndicator={false}
+                        keyExtractor={(item) => item.post_id}
+                        extraData={this.state.data}
+                        ListEmptyComponent={this._listEmptyComponent}
+                        data={this.state.data}
+                        renderItem={({ item }) => {
+                            return(
+                                <View style = {styles.postContainer}>
+                                    <TouchableWithoutFeedback>
+                                        <View>
+                                            <Text style={{fontSize: 10, color: '#cccccc'}}>{item.timestampFront}</Text>
+                                            <Text style={{marginVertical: 6, fontSize: 14}}>{item.content}</Text>
+                                        </View>
+                                    </TouchableWithoutFeedback>
+                                    <View style = {styles.featureContainer}>
+                                        <View style={{flexDirection:'row'}}>
+                                            <TouchableOpacity style={{marginHorizontal: 10}}>
+                                                <Image style={{width: 15, height: 15, resizeMode: 'contain'}}
+                                                       source={that.state.likeIcon}/>
+                                            </TouchableOpacity>
+                                            <Text style = {{color: '#cccccc'}}>{item.like_ctr}</Text>
+                                        </View>
+                                        <View style={{flexDirection:'row'}}>
+                                            <TouchableOpacity style={{marginHorizontal: 10}}>
+                                                <Image style={{width: 15, height: 15, resizeMode: 'contain'}}
+                                                       source={require('../../assets/loveIcon.png')}/>
+                                            </TouchableOpacity>
+                                            <Text style = {{color: '#cccccc'}}>{item.like_ctr}</Text>
+                                        </View>
+                                        <TouchableOpacity style={{marginHorizontal: 10}}>
+                                            <Image style={{width: 15, height: 15, resizeMode: 'contain'}}
+                                                   source={require('../../assets/followIcon.png')}/>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity style={{marginHorizontal: 10}}>
+                                            <Image style={{width: 15, height: 15, resizeMode: 'contain'}}
+                                                   source={require('../../assets/shareIcon.png')}/>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity style={{marginHorizontal: 10}}>
+                                            <Image style={{width: 15, height: 15, resizeMode: 'contain'}}
+                                                   source={require('../../assets/flagIcon.png')}/>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+
+                            )
+                        }
+                        }
+                    />
+                </View>
+            </View>
+
+
+        );
+    }
+}
+const styles = StyleSheet.create({
+    statsText: {fontSize: 30, fontWeight:'bold', color: '#4704a5'},
+    statsView:{
+        flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
+    },
+    featureContainer : {
+        flexDirection: 'row',
+        marginTop: 10,
+        justifyContent: 'space-between'
+    },
+    postContainer:{
+        width: 400,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignContent: 'center',
+        marginBottom: 10,
+        padding: 20,
+        backgroundColor: 'white',
+    },
+    statsContainer:{
+        width: '80%',
+        height: '10%',
+        borderRadius: 20,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+
+    },
+    nameContainer:{
+        backgroundColor: '#4704a5',
+        borderRadius: 20,
+        position: 'relative',
+        width: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 30,
+        top: -15
+    },
+    avatar:{
+        width: 125, height: 125, resizeMode: 'contain', marginTop: '10%'
+    },
+    settingIcon:{
+        width: 35, height: 35, resizeMode: 'contain'
+    },
+
+});
+
---
+++ src/main/resources/static/pages/AccountSettings/SettingScreen.js
@@ -0,0 +1,205 @@
+import React from 'react';
+import {
+    Image,
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    AsyncStorage,
+    Alert,
+} from 'react-native';
+import {deleteAllPostAuth} from "../../fetches/PostFetch";
+import {deleteUser} from "../../fetches/UserFetch";
+
+export default class SettingScreen extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            screenName: '',
+            phoneNumber: '',
+            notificationIcon: require('../../assets/settingNotificationIcon.png'),
+            hide: true
+        }
+    }
+    deleteAllPostHandler = () =>{
+        deleteAllPostAuth(this.state.phoneNumber).then(res => res.text())
+    };
+    deleteUserHandler = () =>{
+        deleteUser(this.state.phoneNumber, this.state.screen).then(res => res.text());
+    };
+    goBack = () =>{
+        this.props.navigation.navigate('AccountScreen');
+    };
+    signOutHandler = async () =>{
+        this.deleteUserHandler();
+        AsyncStorage.clear();
+        this.props.navigation.navigate('Splash');
+    };
+    deleteAccountAlert = () =>
+        Alert.alert(
+            "Are You Sure?",
+            "Deleting your account will not delete any of your posts. By deleting your account" +
+            " you will only unlink your phone number and screen name. In order to delete all post go to settings and" +
+            "click Delete All Post.",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                { text: "OK", onPress: () => this.signOutHandler() }
+            ],
+            { cancelable: false }
+        );
+    deleteAllPostAlert = () =>{
+        Alert.alert(
+            "Are You Sure?",
+            "This will make your post invisible on the feed but if someone is following it, they will still be able to see it.",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                { text: "OK", onPress: () => this.deleteAllPostHandler() }
+            ],
+            { cancelable: false }
+        );
+    };
+    componentDidMount() {
+        AsyncStorage.getItem('screenName').then((token) => {
+            this.setState({
+                screenName: token,
+            });
+        });
+        AsyncStorage.getItem('phoneNumber').then((token) => {
+            this.setState({
+                phoneNumber: token,
+            });
+        });
+    };
+    switchNotiIcon = () =>{
+        if(this.state.notificationIcon === require('../../assets/settingNotificationIcon.png')){
+            this.setState({ notificationIcon: require('../../assets/settingNotificationOff.png')})
+        }
+        else {
+            this.setState({ notificationIcon: require('../../assets/settingNotificationIcon.png')})
+        }
+    };
+    render() {
+        const that = this;
+        return(
+            <View style={{ flex: 1}}>
+                <View style={styles.headerContainer}>
+                    <TouchableOpacity style={{ position:'relative', top: '60%', left: 20}} onPress={() => that.goBack()}>
+                        <Image style={{width: 25, height: 25, resizeMode: 'contain'}}
+                               source={require('../../assets/backIcon.png')}/>
+                    </TouchableOpacity>
+                    <View style={styles.SETTING}>
+                        <Text style={{color: 'white', fontWeight: 'bold', fontSize: 20}}>SETTINGS</Text>
+                    </View>
+                </View>
+                <View style={{padding: 20}}>
+                    <Text style={{color: 'gray'}}>Personal Information</Text>
+                </View>
+                <TouchableOpacity style={styles.sectionContainer} onPress={() => that.props.navigation.navigate('NameScreen')}>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                        <Image style={{width: 35, height: 35, resizeMode: 'contain', marginRight: 20}}
+                               source={require('../../assets/settingName.png')}/>
+                        <Text>{that.state.screenName}</Text>
+                    </View>
+                    <View style={{right: 0, backgroundColor: 'white'}}>
+                        <Image style={{width: 15, height: 15, resizeMode: 'contain'}}
+                               source={require('../../assets/rightIcon.png')}/>
+                    </View>
+                </TouchableOpacity>
+                <View style={styles.sectionContainer}>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                        <Image style={{width: 35, height: 35, resizeMode: 'contain', marginRight: 20}}
+                               source={require('../../assets/settingPhoneIcon.png')}/>
+                        <Text>{that.state.phoneNumber}</Text>
+                    </View>
+                    <View style={{right: 0, backgroundColor: 'white'}}>
+                        <Image style={{width: 15, height: 15, resizeMode: 'contain'}}
+                               source={require('../../assets/rightIcon.png')}/>
+                    </View>
+                </View>
+                <View style={{padding: 20}}>
+                    <Text style={{color: 'gray'}}>Phone Setting</Text>
+                </View>
+                <View style={styles.sectionContainer}>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                        <TouchableOpacity onPress={() => that.switchNotiIcon()}>
+                            <Image style={{width: 35, height: 35, resizeMode: 'contain', marginRight: 20}}
+                                   source={that.state.notificationIcon}/>
+                        </TouchableOpacity>
+                        <Text>Notification</Text>
+                    </View>
+                </View>
+                <View style={{padding: 20}}>
+                    <Text style={{color: 'gray'}}>Account</Text>
+                </View>
+                <TouchableOpacity style={styles.sectionContainer} onPress={() => that.deleteAllPostAlert()}>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                        <Image style={{width: 33, height: 33, resizeMode: 'contain', marginRight: 20}}
+                                   source={require('../../assets/deletePostIcon.png')}/>
+                        <Text>Delete All Post</Text>
+                    </View>
+                    <View style={{right: 0, backgroundColor: 'white'}}>
+                        <Image style={{width: 15, height: 15, resizeMode: 'contain'}}
+                               source={require('../../assets/rightIcon.png')}/>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.sectionContainer} onPress={()=> that.deleteAccountAlert()}>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Image style={{width: 35, height: 35, resizeMode: 'contain', marginRight: 20}}
+                                   source={require('../../assets/deleteAccountIcon.png')}/>
+                        <Text>Delete Account</Text>
+                    </View>
+                    <View style={{right: 0, backgroundColor: 'white'}}>
+                        <Image style={{width: 15, height: 15, resizeMode: 'contain'}}
+                               source={require('../../assets/rightIcon.png')}/>
+                    </View>
+                </TouchableOpacity>
+                <View style={styles.signOutContainer}>
+                    <TouchableOpacity style={styles.signOut} onPress={() => that.signOutHandler()}>
+                        <Text style={{color: 'white'}}>Sign Out</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        backgroundColor: '#4704a5',
+        width: '100%',
+        height: '13%',
+    },
+    SETTING:{
+        alignItems: 'center',
+        top: '25%'
+    },
+    sectionContainer: {
+        paddingHorizontal: 30,
+        paddingTop: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'},
+    signOutContainer:{
+        position:'absolute',
+        bottom: 20,
+        width: '100%',
+        alignItems: 'center'
+    },
+    signOut:{
+        width: '20%',
+        backgroundColor: 'red',
+        alignItems: 'center',
+        borderRadius: 20,
+        height: 30,
+        justifyContent: 'center'
+    }
+});
+
